b087bbfa8955de8d2fb6356aa34c385af8f79432
qiuhb <2875666506@qq.com>
2022-09-08 11:21:34 +0800

    Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory)

--- 数值分析/上机作业/main.py
+++ 数值分析/上机作业/main.py
@@ -5,7 +5,7 @@ def accumulate(n):
         Forward += 1/(i**2 - 1)
     for i in range(n, 1, -1):
         Reverse += 1/(i**2 - 1)
-    print('The N is set as:', n, '\nThe accurate value is: Sn =', Sn,
+    print('The N is set as:', n, '\nThe accurate value is: Sn =', float(Sn),
           '\nThe result in forward order is: Forward=', Forward, '\nThe result in reverse order is: Reverse=', Reverse)
     print('Error in Forward order: |Sn-Forward|=', abs(Sn - Forward), '\nError in reverse order: |Sn - Reverse|=', abs(Sn - Reverse))
 if __name__ == '__main__':
