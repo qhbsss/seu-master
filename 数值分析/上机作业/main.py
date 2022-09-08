7ab0bdd6d5ac96489485947d54efa4834f803bf8
qiuhb <2875666506@qq.com>
2022-09-08 11:15:24 +0800

    ErrorAnalyze

---
+++ 数值分析/上机作业/main.py
@@ -0,0 +1,14 @@
+def accumulate(n):
+    Sn = 1/2*(3/2 - 1/n - 1/(n+1))
+    Forward, Reverse = 0, 0
+    for i in range(2, n+1):
+        Forward += 1/(i**2 - 1)
+    for i in range(n, 1, -1):
+        Reverse += 1/(i**2 - 1)
+    print('The N is set as:', n, '\nThe accurate value is: Sn =', Sn,
+          '\nThe result in forward order is: Forward=', Forward, '\nThe result in reverse order is: Reverse=', Reverse)
+    print('Error in Forward order: |Sn-Forward|=', abs(Sn - Forward), '\nError in reverse order: |Sn - Reverse|=', abs(Sn - Reverse))
+if __name__ == '__main__':
+     N = [100, 10000, 1000000]
+     for i in N:
+         accumulate(i)
